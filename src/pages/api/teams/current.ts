0c8f5397e2b0a3d0c67d66aa3cb1e9ffc7419cff
Bartłomiej Rudecki <przekichane@icloud.com>
2025-01-11 00:50:29 +0100

    feat(/api/teams/current): support GET

--- src/pages/api/teams/current.ts
+++ src/pages/api/teams/current.ts
@@ -1,5 +1,14 @@
 import type { APIRoute } from "astro";
 
+export const GET: APIRoute = async ({ cookies, request, redirect }) => {
+  if (!cookies.has("current_team")) {
+    return new Response(null);
+  }
+  const currentTeam = cookies.get("current_team")?.value;
+
+  return new Response(currentTeam);
+};
+
 export const POST: APIRoute = async ({ cookies, request, redirect }) => {
   const form_data = await request.formData();
 
