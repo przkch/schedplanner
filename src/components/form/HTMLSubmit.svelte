64f39439ee466cd8c8768c76ab3d41b23c5a2fc7
Bartłomiej Rudecki <przekichane@icloud.com>
2025-01-11 01:02:59 +0100

    feat(components/form/HTMLSubmit): allow type and onclick props

--- src/components/form/HTMLSubmit.svelte
+++ src/components/form/HTMLSubmit.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+  import type { MouseEventHandler } from "svelte/elements";
+
   interface Props {
     class?: string;
     disabled?: boolean;
     label?: string;
+    type?: "button" | "submit" | undefined;
+    onclick: MouseEventHandler<HTMLButtonElement>;
   }
 
   const props: Props = $props();
@@ -11,8 +15,9 @@
 <div class={["flex justify-center sm:justify-end", props.class]}>
   <button
     class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md enabled:hover:bg-gray-600 focus:outline-none enabled:focus:bg-gray-600 disabled:opacity-75"
-    type="submit"
+    type={props.type || "submit"}
     disabled={props.disabled}
+    onclick={props.onclick}
   >
     {props.label}
   </button>
