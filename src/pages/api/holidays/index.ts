7062fa19358818f27abfb954692f2723ee10cdde
Bartłomiej Rudecki <przekichane@icloud.com>
2025-01-11 16:10:10 +0100

    feat(api/holdays): allow GET

--- src/pages/api/holidays/index.ts
+++ src/pages/api/holidays/index.ts
@@ -3,6 +3,16 @@ import { holiday } from "@lib/database/schema";
 
 import type { APIRoute } from "astro";
 
+export const GET: APIRoute = async () => {
+  const holidays = await db.select().from(holiday).orderBy(holiday.date);
+
+  return new Response(JSON.stringify(holidays), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
 export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
 
