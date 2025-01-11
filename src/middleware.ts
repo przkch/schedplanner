db5c1a42da8f90fc68b907a3e0d05fa2862f520c
Bartłomiej Rudecki <przekichane@icloud.com>
2025-01-11 14:49:25 +0100

    chore(middleware): do not set language tag on api routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,10 @@ import type { AvailableLanguageTag } from "@paraglide/runtime";
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
+  const isApiRoute = context.url.pathname.startsWith("/api");
+
+  if (isApiRoute) return next();
+
   setLanguageTag((context.preferredLocale || "en") as AvailableLanguageTag);
 
   return next();
