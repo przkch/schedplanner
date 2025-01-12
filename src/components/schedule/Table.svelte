2858e8cecaca6daa721bf7943470cc7b9d60d8aa
Bartłomiej Rudecki <przekichane@icloud.com>
2025-01-12 22:08:13 +0100

    fix(components/schedule/Table): try to open modal only if it's connected

--- src/components/schedule/Table.svelte
+++ src/components/schedule/Table.svelte
@@ -218,7 +218,7 @@
           return;
         }
 
-        if (!modifyScheduleElement.open) {
+        if (modifyScheduleElement.isConnected && !modifyScheduleElement.open) {
           modifyScheduleElement.showModal();
         }
       };
